03aca8ddd3f1d0ca29b8d85971b3f6493a65b7b5
Tien <annhgch18718@fpt.edu.vn>
2023-03-03 01:58:59 +0700

    first commit

---
+++ problem2/arr.js
@@ -0,0 +1,25 @@
+let button = document.querySelector("button");
+button.addEventListener("click", function () {
+  let input = document.querySelector("input").value;
+  if (isNumeric(input)) {
+    let result = generator_random(input);
+    if (result) {
+      let output = document.querySelector(".generator-result");
+      output.innerHTML = `[${result}]`;
+    }
+  }
+});
+const isNumeric = (num) =>
+  (typeof num === "number" || (typeof num === "string" && num.trim() !== "")) &&
+  !isNaN(num);
+const generator_random = (num) => {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push(randomNumber(1, num));
+  }
+  return arr;
+};
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
