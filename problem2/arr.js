118a3a0b6edcd7b55e97c683fb4b5c9974c096a6
nguyen.viet.tien <nguyen.viet.tien@mqsolutions.com.vn>
2023-03-06 15:18:53 +0700

    third commit

--- problem2/arr.js
+++ problem2/arr.js
@@ -1,16 +1,16 @@
-const isNumeric = (num) =>
+const isNumeric = (num) => {
     (typeof num === "number" || (typeof num === "string" && num.trim() !== "")) &&
     !isNaN(num);
+};
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 const generator_random = (num) => {
     var arr = [];
     for (var i = 0; i < num; i++) {
         arr.push(randomNumber(1, num));
     }
     return arr;
-};
-
-const randomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 let button = document.querySelector("button");
